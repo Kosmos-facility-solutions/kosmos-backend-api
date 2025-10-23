fd9e65dd0a4fa5d888dc5f38d18884534b0d890e
ArgenisGonzalez <arggon186@gmail.com>
2025-10-23 14:22:24 -0400

    Rename servicerequest to serviceRequest

---
+++ src/modules/serviceRequest/dto/update-service-request.dto.ts
@@ -0,0 +1,102 @@
+import { Transform } from 'class-transformer';
+import {
+  IsArray,
+  IsBoolean,
+  IsDateString,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Min,
+} from 'class-validator';
+import {
+  RecurrenceFrequency,
+  ServiceRequestPriority,
+  ServiceRequestStatus,
+} from '../entities/serviceRequest.entity';
+
+export class UpdateServiceRequestDto {
+  @IsOptional()
+  @IsNumber()
+  propertyId?: number;
+
+  @IsOptional()
+  @IsNumber()
+  serviceId?: number;
+
+  @IsOptional()
+  @IsEnum(ServiceRequestStatus)
+  status?: ServiceRequestStatus;
+
+  @IsOptional()
+  @IsEnum(ServiceRequestPriority)
+  priority?: ServiceRequestPriority;
+
+  @IsOptional()
+  @IsDateString()
+  @Transform(({ value }) => (value ? new Date(value) : undefined), {
+    toClassOnly: true,
+  })
+  scheduledDate?: Date;
+
+  @IsOptional()
+  @IsString()
+  scheduledTime?: string;
+
+  @IsOptional()
+  @IsDateString()
+  @Transform(({ value }) => (value ? new Date(value) : undefined), {
+    toClassOnly: true,
+  })
+  completedDate?: Date;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  estimatedPrice?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  actualPrice?: number;
+
+  @IsOptional()
+  @IsString()
+  notes?: string;
+
+  @IsOptional()
+  @IsString()
+  specialInstructions?: string;
+
+  @IsOptional()
+  @IsString()
+  cancellationReason?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isRecurring?: boolean;
+
+  @IsOptional()
+  @IsEnum(RecurrenceFrequency)
+  recurrenceFrequency?: RecurrenceFrequency;
+
+  @IsOptional()
+  @IsDateString()
+  @Transform(({ value }) => (value ? new Date(value) : undefined), {
+    toClassOnly: true,
+  })
+  recurrenceEndDate?: Date;
+
+  @IsOptional()
+  @IsNumber()
+  estimatedDurationMinutes?: number;
+
+  @IsOptional()
+  @IsNumber()
+  actualDurationMinutes?: number;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  additionalServices?: string[];
+}
---
+++ src/modules/serviceRequest/servicerequest.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ServiceRequest } from './entities/serviceRequest.entity';
+import { ServiceRequestController } from './serviceRequest.controller';
+import { ServiceRequestRepository } from './serviceRequest.repository';
+import { ServiceRequestService } from './serviceRequest.service';
+
+@Module({
+  imports: [SequelizeModule.forFeature([ServiceRequest])],
+  controllers: [ServiceRequestController],
+  providers: [ServiceRequestService, ServiceRequestRepository],
+  exports: [SequelizeModule, ServiceRequestService, ServiceRequestRepository],
+})
+export class ServiceRequestModule {}
